e1c65f5c2a052aa972c9885ef60b5c5cd94dc63d
ankahara <hecatombe100@gmail.com>
2025-07-10 23:35:28 -0600

    Refactor code structure for improved readability and maintainability

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,11 +7,14 @@ import db from '@astrojs/db';
 
 import auth from 'auth-astro';
 
+import vercel from '@astrojs/vercel';
+
 // https://astro.build/config
 export default defineConfig({
   vite: {
     plugins: [tailwindcss()]
   },
 
-  integrations: [db()]
+  integrations: [db()],
+  adapter: vercel()
 });
