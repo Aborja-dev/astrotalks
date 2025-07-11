907a57d5e5ddf529b9f9e827bcbe0236f58b4ec3
ankahara <hecatombe100@gmail.com>
2025-07-10 23:55:02 -0600

    agregar el apth resolve a astro config para deploy en vercel

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,9 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
-
+import path from 'node:path'
+import url from 'node:url'
 import tailwindcss from '@tailwindcss/vite';
-
+const _dirname = path.dirname(url.fileURLToPath(import.meta.url))
 import db from '@astrojs/db';
 
 import auth from 'auth-astro';
@@ -12,9 +13,11 @@ import vercel from '@astrojs/vercel';
 // https://astro.build/config
 export default defineConfig({
   vite: {
-    resolve: { alias: {
-      '@': './src'
-    } },
+    resolve: {
+      alias: {
+        "@": path.resolve(_dirname, "./src")
+      }
+    },
     plugins: [tailwindcss()]
   },
 
